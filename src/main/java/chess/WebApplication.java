7253a7f0b5fd1cd3e8676cdc3d898bf073e9b1fc
S. J. Kang <83529599+sojukang@users.noreply.github.com>
2022-04-03 03:30:42 +0900

    feat: 체스판과 기물들 위치 및 이동 이미지로 구현

--- src/main/java/chess/WebApplication.java
+++ src/main/java/chess/WebApplication.java
@@ -4,10 +4,13 @@ import static spark.Spark.*;
 
 import java.util.HashMap;
 import java.util.Map;
+import java.util.Objects;
 import java.util.stream.Collectors;
 
 import chess.model.Board;
+import chess.model.File;
 import chess.model.Position;
+import chess.model.Rank;
 import chess.model.TurnDecider;
 import chess.model.boardinitializer.defaultInitializer;
 import chess.model.piece.Piece;
@@ -55,11 +58,44 @@ public class WebApplication {
 
     private static Map<String, String> StringMapByBoardValues(Board board) {
         Map<Position, Piece> values = board.getValues();
-        return values.keySet().stream().collect(Collectors.toUnmodifiableMap(
-            Position::getString, key -> values.get(key).getEmblem()));
+        Map<String, String> result = new HashMap<>();
+        for (Rank rank : Rank.reverseValues()) {
+            for (File file : File.values()) {
+                result.put(Position.of(rank, file).getString(),
+                    eachPieceEmblem(values, Position.of(rank, file)));
+            }
+        }
+        return result;
     }
 
     private static String render(Map<String, Object> model, String templatePath) {
         return new HandlebarsTemplateEngine().render(new ModelAndView(model, templatePath));
+    }
+
+    private static String eachPieceEmblem(Map<Position, Piece> piecesByPositions, Position position) {
+        if (Objects.isNull(piecesByPositions.get(position))) {
+            return "empty";
+        }
+
+        return emblemMapper(piecesByPositions.get(position).getEmblem());
+    }
+
+    private static String emblemMapper(String emblem) {
+        Map<String, String> mappingTable = new HashMap<>();
+        mappingTable.put("p", "blackPawn");
+        mappingTable.put("r", "blackRook");
+        mappingTable.put("n", "blackKnight");
+        mappingTable.put("b", "blackBishop");
+        mappingTable.put("q", "blackQueen");
+        mappingTable.put("k", "blackKing");
+
+        mappingTable.put("P", "whitePawn");
+        mappingTable.put("R", "whiteRook");
+        mappingTable.put("N", "whiteKnight");
+        mappingTable.put("B", "whiteBishop");
+        mappingTable.put("Q", "whiteQueen");
+        mappingTable.put("K", "whiteKing");
+
+        return mappingTable.get(emblem);
     }
 }
